c86a8ad8b5374fb59264271ee9489b38ed8b1681
xmonkey02 <65108714+xmonkey02@users.noreply.github.com>
2022-12-05 00:16:12 +0800

    20221205

--- Rustic-Pages-master/ECrepot/index.js
+++ Rustic-Pages-master/ECrepot/index.js
@@ -1,13 +1,49 @@
 var TmpVal = {
   bookStock: [
-        { Rkey: 1, Title: "Chase The Sun", Author: "Martin", ISBN: "", Year: "2008" },
-        { Rkey: 2, Title: "Chase The Sun", Author: "Alan", ISBN: "", Year: "2020" },
-        { Rkey: 3, Title: "Chase The Sun", Author: "Judith", ISBN: "", Year: "2010" },
+    {
+      Rkey: 1,
+      Title: "Chase The Sun",
+      Author: "Martin",
+      ISBN: "978-957-511-785-6",
+      Year: "2008",
+    },
+    {
+      Rkey: 2,
+      Title: "Fight with The Sun",
+      Author: "Alan",
+      ISBN: "978-957-511-785-7",
+      Year: "2020",
+    },
+    {
+      Rkey: 3,
+      Title: "Play with The Sun",
+      Author: "Judith",
+      ISBN: "978-957-511-785-8",
+      Year: "2010",
+    },
   ],
   Users: [
-    { UserName: "Martin", PassWord: "Abc123", Rkey: 1 },
-    { UserName: "Alan", PassWord: "HandsomeBoy", Rkey: 2 },
-    { UserName: "Judith", PassWord: "IamSoGood", Rkey: 3 },
+    {
+      UserName: "Martin",
+      PassWord: "0",
+      Rkey: 1,
+      FirstName: "品堯",
+      LastName: "黃",
+    },
+    {
+      UserName: "Alan",
+      PassWord: "HandsomeBoy",
+      Rkey: 2,
+      FirstName: "弘騰",
+      LastName: "沈",
+    },
+    {
+      UserName: "Judith",
+      PassWord: "IamSoGood",
+      Rkey: 3,
+      FirstName: "文宸",
+      LastName: "李",
+    },
   ],
 };
 
@@ -37,10 +73,11 @@ function regist_onclick() {
       let Valid = false;
 
       TmpVal.Users.forEach(function (element) {
-        if (element.UserName == Account) {
+        if (element.UserName.toUpperCase() == Account.toUpperCase()) {
           if (element.PassWord == PWD) {
             Valid = true;
             $("#hf_Rkey").val(element.Rkey);
+            setUser(element.Rkey);
           }
         }
       });
@@ -71,9 +108,51 @@ function regist_onclick() {
         let Type = $('#sl2_type').val();
         let Param = $('#txt_search').val();
         //如果搜尋有結果就把notice隱藏起來
-        $('#div_Notice').hide();
-        //把結果打開
-        $('#div_result').show();
+        let fulfill = [];
+        switch (Type) {
+          case "Title":
+            TmpVal.bookStock.forEach(function (element) {
+              if (element.Title.toUpperCase().includes(Param.toUpperCase())) {
+                fulfill.push(element.Rkey);
+              }
+            });
+            break;
+          case "Author":
+            TmpVal.bookStock.forEach(function (element) {
+              if (element.Author.toUpperCase().includes(Param.toUpperCase())) {
+                fulfill.push(element.Rkey);
+              }
+            });
+            break;
+          case "ISBN":
+            TmpVal.bookStock.forEach(function (element) {
+              if (element.ISBN.toUpperCase().includes(Param.toUpperCase())) {
+                fulfill.push(element.Rkey);
+              }
+            });
+            break;
+        }
+        //表示有找到東西
+        if (fulfill.length > 0) {
+          //關閉提示訊息
+          $("#div_Notice").hide();
+          //把結果打開
+          $("#div_result").show();
+          let innerHTML = '';
+          fulfill.forEach(function (pickedKey) {
+            TmpVal.bookStock.forEach(function (books) {
+              if (books.Rkey == pickedKey) {
+                innerHTML += '<div class="card"><div class="context"><div class="card-body"><div class="col-12 col-md-auto" style="width: 100%;"><div class="row"><h2><a href="#" onclick="">'+books.Title+'</a></h2></div><div class="row">by&ensp;<a href="#">'+books.Author+'</a></div><div class="row">published '+books.Year+'</div></div></div></div></div>';
+              }
+            })
+          });
+          $('#div_result').html(innerHTML);
+        }
+        else{
+          $("#div_Notice").html('<h2 style="margin-top: 100px;">搜尋不到您要的書，請重新搜尋。</h2>');
+        }
+
+        
     });
 }
 
@@ -86,6 +165,17 @@ function Default() {
     $('#div_main').show();
     $('#div_Notice').show();
     $('#div_result').hide();
+}
+
+//設定使用者名稱到相對應的欄位
+function setUser(Rkey) {
+  TmpVal.Users.forEach(function (element) {
+    if (element.Rkey == Rkey) {
+      $("#lb_showName").text("Hi," + element.FirstName);
+      let content = '<h2 style="margin-top: 100px;"> 歡迎 <label style="font-weight: bold;">'+element.FirstName+'</label> 讀者，請使用搜尋列尋找你要的書!</h2> ';
+      $("#div_Notice").html(content);
+    }
+  });
 }
 
 function bookShow(bookRkey) {
