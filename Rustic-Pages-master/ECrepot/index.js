5b66336a3636196e943c22768f4d70b2ecf95c12
xmonkey02 <65108714+xmonkey02@users.noreply.github.com>
2022-12-04 19:26:31 +0800

    update

--- Rustic-Pages-master/ECrepot/index.js
+++ Rustic-Pages-master/ECrepot/index.js
@@ -1,3 +1,16 @@
+var TmpVal = {
+  bookStock: [
+        { Rkey: 1, Title: "Chase The Sun", Author: "Martin", ISBN: "", Year: "2008" },
+        { Rkey: 2, Title: "Chase The Sun", Author: "Alan", ISBN: "", Year: "2020" },
+        { Rkey: 3, Title: "Chase The Sun", Author: "Judith", ISBN: "", Year: "2010" },
+  ],
+  Users: [
+    { UserName: "Martin", PassWord: "Abc123", Rkey: 1 },
+    { UserName: "Alan", PassWord: "HandsomeBoy", Rkey: 2 },
+    { UserName: "Judith", PassWord: "IamSoGood", Rkey: 3 },
+  ],
+};
+
 $(function () {
     // prog_ID = "GES0051";
     // if (url.indexOf("csmc.com.tw") > 0) {
@@ -18,12 +31,31 @@ $(function () {
 function regist_onclick() {
     // 登入按鈕
     $('#btn_Login').on('click',function () {
+      //比對是否正確
+      let Account = $("#txt_LogAccount").val();
+      let PWD = $("#txt_LogPWD").val();
+      let Valid = false;
+
+      TmpVal.Users.forEach(function (element) {
+        if (element.UserName == Account) {
+          if (element.PassWord == PWD) {
+            Valid = true;
+            $("#hf_Rkey").val(element.Rkey);
+          }
+        }
+      });
+
+      if (Valid) {
         //畫面切換
-        $('#div_Login').hide();
-        $('#div_regist').hide();
-        $('#div_AfterLogin').show();
-        $('#div_search').show();
+        $("#div_Login").hide();
+        $("#div_regist").hide();
+        $("#div_AfterLogin").show();
+        $("#div_search").show();
         Default();
+      }
+      else {
+        alert("You have either wrong Account or Password, Please check again.");
+        }
     });
     //登出按鈕
     $('#btn_LogOut').on('click',function () {
@@ -54,4 +86,25 @@ function Default() {
     $('#div_main').show();
     $('#div_Notice').show();
     $('#div_result').hide();
+}
+
+function bookShow(bookRkey) {
+    //先判斷是否有這本書
+    let haveBook = false;
+    TmpVal.bookStock.forEach(function (book) {
+        if (book.Rkey == bookRkey) {
+            haveBook = true;
+        }
+    })
+    if (haveBook) {
+        
+    }
+    switch (bookRkey) {
+        case 1:
+            
+            break;
+    
+        default:
+            break;
+    }
 }
