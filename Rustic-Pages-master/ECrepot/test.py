f97be85dca72c444647cf974cfc8b6a0cadc6ae6
judith <judithwlee.ee04@g2.nctu.edu.tw>
2022-12-06 20:37:38 +0800

    [test] edit test.py
    Usage:
    - use "/test" in your url when calling the function with ajax
    - use POST, you can also try to use "GET" and see what happens
    
    Change:
    import flask
    
    Reason:
    Previous attempts have failed

--- Rustic-Pages-master/ECrepot/test.py
+++ Rustic-Pages-master/ECrepot/test.py
@@ -1,3 +1,12 @@
+#!/usr/bin/env python
+"""
+this module exists to figure out how to get python to listen to js
+"""
+
+from flask import Flask, jsonify, request
+
+app = Flask(__name__)
+
 def dummy_true() -> bool:
     """
     This function does nothing but return True
@@ -16,22 +25,40 @@ def dummy_zero() -> int:
     """
     return 0
 
-def main(*args):
+def function_router(input_data: list):
     """
     A function to return either dummy_true(), dummy_false(), or dummy_zero()
     based on the first input
     """
-    print(f"Input type: {type(args[0])}")
-    if isinstance(args[0], str):
-        if args[0].lower() == "true":
+    print(f"Input type: {type(input_data[0])}")
+    if isinstance(input_data[0], str):
+        if input_data[0].lower() == "true":
             return dummy_true()
-        if args[0].lower() == "false":
+        if input_data[0].lower() == "false":
             return dummy_false()
-    if isinstance(args[0], bool):
-        if args[0]:
+    if isinstance(input_data[0], bool):
+        if input_data[0]:
             return dummy_true()
         return dummy_false()
     return dummy_zero()
 
+@app.route('/test', methods=['POST', 'GET'])
+def route():
+    """
+    this function listens to the frontend
+    """
+    if request.method == "POST":
+        data = request.get_json()
+        parameters = []
+        for item in data:
+            parameters.append(data[item])
+        return jsonify(function_router(parameters))
+
+    if request.method == "GET":
+        print("GET not supported!")
+        return None
+    return None
+
+
 if __name__ == "__main__":
-    main()
+    pass
